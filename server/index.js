c8cc44669b5436af3f0d16c421abe07010e70b19
hschmaiske <ishenriquealbert@gmail.com>
2022-11-15 15:17:58 -0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+// Require the framework and instantiate it
+
+const fastify = require('fastify')({
+  logger: true,
+})
+
+// Declare a route
+fastify.get('/', (request, reply) => {
+  reply.send({ hello: 'world' })
+})
+
+// Run the server!
+fastify.listen({ port: 3000 }, (err, address) => {
+  if (err) throw err
+  // Server is now listening on ${address}
+})
